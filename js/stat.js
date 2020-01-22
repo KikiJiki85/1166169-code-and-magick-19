eeda4e4961cacccf5264e9aa7915e24e85a7e17d
Aleksey Vaskin <alzor85@gmail.com>
2020-01-22 17:19:56 +0300

    Завершает Учебный проект - дз

--- js/stat.js
+++ js/stat.js
@@ -3,15 +3,14 @@
 // times — массив, по длине совпадающий с массивом names. Массив содержит время прохождения уровня соответствующего игрока из массива names. Время прохождения уровня задано в миллисекундах.
 'use strict';
 
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 50;
+var CLOUD_Y = 10;
 var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_GAP = 50;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -28,20 +27,31 @@ var getMaxElement = function (arr) {
     }
     return maxElement;
   } else {
-    return 'Вы передали пустой массиe';
+    return 'Вы передали пустой массив';
   }
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+  ctx.font = '16px "PT Mono"';
   ctx.fillStyle = '#000';
+  ctx.fillText('Ура вы победили!', CLOUD_X + 20, CLOUD_Y + 30);
+  ctx.fillText('Список результатов:', CLOUD_X + 20, CLOUD_Y + 50);
+
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT - CLOUD_Y);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, 230 - (BAR_HEIGHT * times[i] / maxTime));
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1';
+    } else {
+      ctx.fillStyle = 'hsl( 235,' + Math.floor(Math.random() * 100) + '%, 50%)';
+    }
+    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - BAR_WIDTH, BAR_WIDTH, -(BAR_HEIGHT * times[i] / maxTime));
   }
 };
 
