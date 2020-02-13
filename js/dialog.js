5e6c67843a0fd40fe07dedc09ca7a96fcc37082f
Aleksey Vaskin <alzor85@gmail.com>
2020-02-13 16:03:20 +0300

    Завершает дз 11

--- js/dialog.js
+++ js/dialog.js
@@ -1,8 +1,13 @@
 'use strict';
 (function () {
+  var ESC_KEY = 'Escape';
 
   var setupDialogElement = document.querySelector('.setup');
   var dialogHandler = setupDialogElement.querySelector('.upload');
+  var userNameInput = setupDialogElement.querySelector('.setup-user-name');
+  var setupSimilar = document.querySelector('.setup-similar');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setupDialogElement.querySelector('.setup-close');
 
   dialogHandler.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
@@ -53,5 +58,65 @@
     document.addEventListener('mouseup', onMouseUp);
   });
 
+  var onFormEscKeydown = function (evt) {
+    if (evt.key === ESC_KEY && !(userNameInput === document.activeElement)) {
+      closePopup();
+    }
+  };
+  // Функция показа окна персонажа
+  var openPopup = function () {
+    setupDialogElement.classList.remove('hidden');
+    document.addEventListener('keydown', onFormEscKeydown);
+    window.colorize.wizardCoat.addEventListener('click', window.colorize.onWizardCoatClick);
+    window.colorize.wizardEyes.addEventListener('click', window.colorize.onWizardEyesClick);
+    window.colorize.setupFireball.addEventListener('click', window.colorize.onFireballClick);
+  };
+
+  // Функция скрытия окна персонажа
+  var closePopup = function () {
+    setupDialogElement.classList.add('hidden');
+    document.removeEventListener('keydown', onFormEscKeydown);
+    window.colorize.wizardCoat.removeEventListener('click', window.colorize.onWizardCoatClick);
+    window.colorize.wizardEyes.removeEventListener('click', window.colorize.onWizardEyesClick);
+    window.colorize.setupFireball.removeEventListener('click', window.colorize.onFireballClick);
+    resetDialogPosition();
+  };
+
+  var resetDialogPosition = function () {
+    setupDialogElement.style.top = '';
+    setupDialogElement.style.left = '';
+  };
+
+  // Перевод ошибок ввода на русский язык - событие invalid
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      // Самое главное при работе с обработчиками валидации — не забыть сбросить значение поля, если это значение стало корректно.
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  setupSimilar.classList.remove('hidden');
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -1,79 +1,8 @@
+
 'use strict';
-var namesArr = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
-
-var surnamesArr = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
-
-var coatColorArr = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-
-var eyesColorArr = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
-
-var fireballColorArr = [
-  '#ee4830',
-  '#30a8ee',
-  '#5ce6c0',
-  '#e848d5',
-  '#e6e848'
-];
-
 var wizardTemplate = document.querySelector('#similar-wizard-template').content;
-
-// Функция выбора случайного элемента массива
-var getArrRandomElement = function (arr) {
-  var randomIndex = Math.floor(Math.random() * arr.length);
-  return arr[randomIndex];
-};
-
-// Функция выбора случайного имени и фамилии
-var generateName = function (names, surnames) {
-  var randomName = getArrRandomElement(names);
-  var randomSurname = getArrRandomElement(surnames);
-  return randomName + ' ' + randomSurname;
-};
-
-// Функция создания массива из 4х случайных волшебников
-var createWizardsArr = function () {
-  var array = [];
-  for (var i = 0; i < 4; i++) {
-    var wizardObj = {
-      name: generateName(namesArr, surnamesArr),
-      coatColor: getArrRandomElement(coatColorArr),
-      eyesColor: getArrRandomElement(eyesColorArr)
-    };
-    array[i] = wizardObj;
-  }
-  return array;
-};
+var setupSimilar = document.querySelector('.setup-similar');
+var wizardSimilarList = setupSimilar.querySelector('.setup-similar-list');
 
 // Функция подготовки шаблона и вставки волшебников в canvas
 var renderWizardsList = function (array) {
@@ -87,106 +16,13 @@ var renderWizardsList = function (array) {
     wizardNew.querySelector('.setup-similar-label').textContent = array[i].name;
     wizardNew.querySelector('.wizard-coat').style.fill = array[i].coatColor;
     wizardNew.querySelector('.wizard-eyes').style.fill = array[i].eyesColor;
-    // wizardNew.querySelector('.wizard-coat').setAttribute('fill', array[i].coatColor);
-    // wizardNew.querySelector('.wizard-eyes').setAttribute('fill', array[i].coatColor);
-
     // Вставка волшебника(ноды)
     wizardSimilarList.appendChild(wizardNew);
   }
 };
 
 // Создает массив волшебников
-var wizardsArr = createWizardsArr();
-
-// Переменные для вставки волшебников
-var setupSimilar = document.querySelector('.setup-similar');
-var wizardSimilarList = setupSimilar.querySelector('.setup-similar-list');
+var wizardsArr = window.data.createWizardsArr();
 
 // Отрисовка волшебников
 renderWizardsList(wizardsArr);
-
-// Показывает блок похожих персонажей
-setupSimilar.classList.remove('hidden');
-
-// Работа с событиями
-var ESC_KEY = 'Escape';
-var ENTER_KEY = 'Enter';
-var setupBlock = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setupBlock.querySelector('.setup-close');
-var userNameInput = setupBlock.querySelector('.setup-user-name');
-
-var popupEscPressHandler = function (evt) {
-  if (evt.key === ESC_KEY && !(userNameInput === document.activeElement)) {
-    closePopup();
-  }
-};
-
-var openPopup = function () {
-  setupBlock.classList.remove('hidden');
-  document.addEventListener('keydown', popupEscPressHandler);
-};
-
-var closePopup = function () {
-  setupBlock.classList.add('hidden');
-  document.removeEventListener('keydown', popupEscPressHandler);
-};
-
-// Показывает блок настроек setup Игры
-setupOpen.addEventListener('click', function () {
-  openPopup();
-});
-
-setupOpen.addEventListener('keydown', function (evt) {
-  if (evt.key === ENTER_KEY) {
-    openPopup();
-  }
-});
-
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
-
-setupClose.addEventListener('keydown', function (evt) {
-  if (evt.key === ENTER_KEY) {
-    closePopup();
-  }
-});
-
-// Перевод ошибок ввода на русский язык - событие invalid
-userNameInput.addEventListener('invalid', function () {
-  if (userNameInput.validity.tooShort) {
-    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-  } else if (userNameInput.validity.tooLong) {
-    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
-  } else if (userNameInput.validity.valueMissing) {
-    userNameInput.setCustomValidity('Обязательное поле');
-  } else {
-    // Самое главное при работе с обработчиками валидации — не забыть сбросить значение поля, если это значение стало корректно.
-    userNameInput.setCustomValidity('');
-  }
-});
-
-// Изменения цвета мантии персонажа по нажатию.
-var wizardCoat = setupBlock.querySelector('.wizard-coat');
-var wizardEyes = setupBlock.querySelector('.wizard-eyes');
-var setupFireball = setupBlock.querySelector('.setup-fireball-wrap');
-
-wizardCoat.addEventListener('click', function () {
-  var playerWizardCoatColor = getArrRandomElement(coatColorArr);
-  wizardCoat.style.fill = playerWizardCoatColor;
-  setupBlock.querySelector('.setup-player').querySelector('input[name="coat-color"]').value = playerWizardCoatColor;
-});
-
-wizardEyes.addEventListener('click', function () {
-  var playerWizardEyesColor = getArrRandomElement(eyesColorArr);
-  wizardEyes.style.fill = playerWizardEyesColor;
-  setupBlock.querySelector('.setup-player').querySelector('input[name="eyes-color"]').value = playerWizardEyesColor;
-
-});
-
-setupFireball.addEventListener('click', function () {
-  var playerFireballColor = getArrRandomElement(fireballColorArr);
-  setupFireball.style.background = playerFireballColor;
-  setupFireball.querySelector('input').value = playerFireballColor;
-});
